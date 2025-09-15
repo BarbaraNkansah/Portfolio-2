8d625c5206bd63ed5b6aa3a5d501ad26766234e4
Barbara Nkansah <barbaraoparebea8@gmail.com>
2025-09-15 23:07:54 +0000

    Update Navbar for responsiveness fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Menu, X } from "lucide-react";
 import { Link } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
 // Hamburger (Menu) & Close (X) icons
 
 function Navbar() {
@@ -8,9 +9,10 @@ function Navbar() {
 
   return (
     
-      <header className="fixed top-0 left-0 w-full z-50 bg-black px-20 py-2">
-        <div className="flex mx-auto justify-between items-center m-8 text-xl font-serif bg-black text-white rounded-full md:shadow-lg">
-          <h1 className="ml-20">
+      <header className="fixed top-0 left-0 w-full z-50 bg-black px-4 py-2">
+        <div className="flex justify-between items-center text-lg font-serif text-white rounded-full md:shadow-lg max-w-7xl mx-auto px-6 py-6 md:px-12">
+
+          <h1 className="text-base sm:text-lg md:text-xl font-bold">
            <Link to="/"> BARBARA <span className="text-[#00c9a7]">NKANSAH</span></Link>
           </h1>
 
@@ -19,12 +21,17 @@ function Navbar() {
             {isOpen ? <X size={28} /> : <Menu size={28} />}
           </button>
 
-          <nav
-            className={`${
-              isOpen ? "block" : "hidden"
-            } md:block md:bg-transparent`}
-          >
-            <ul className="flex flex-col gap-4 md:flex-row md:gap-10">
+          <AnimatePresence>
+            {isOpen &&(
+              <motion.nav
+              initial={{opacity:0, y: -20}}
+              animate={{opacity: 1, y: 0}}
+              exist={{opacity:0, y: -20}}
+              transition={{duration: 0.3}}
+              className= "absolute top-14 left-0 w-full bg-black md:hidden"
+             >
+            
+             <ul className="flex flex-col items-center gap-4 md:flex-row md:gap-10 md:py-0">
               <li className="relative cursor-pointer after:content-[''] after:block after:w-0 after:h-[2px] after:bg-[#00c9a7] after:transition-all after:duration-300 hover:after:w-full">
                 
                 <Link to="/project">Projects</Link>
@@ -44,7 +51,36 @@ function Navbar() {
                 CV
               </a>
             </ul>
-          </nav>
+            </motion.nav>
+            )}
+          </AnimatePresence>
+            
+          <nav
+            className={`${
+              isOpen ? "block" : "hidden"
+            }absolute top-full left-0 w-full bg-black md:static md:w-auto md:bg-transparent md:block`}
+          >
+            <ul className="flex flex-col items-center gap-4 md:flex-row md:gap-10 md:py-0">
+              <li className="relative cursor-pointer after:content-[''] after:block after:w-0 after:h-[2px] after:bg-[#00c9a7] after:transition-all after:duration-300 hover:after:w-full">
+                
+                <Link to="/project">Projects</Link>
+              </li>
+              <li className="relative cursor-pointer after:content-[''] after:block after:w-0 after:h-[2px] after:bg-[#00c9a7] after:transition-all after:duration-300 hover:after:w-full">
+                
+                    <Link to="/about">About Me</Link>
+              </li>
+          
+              <li className="relative cursor-pointer after:content-[''] after:block after:w-0 after:h-[2px] after:bg-[#00c9a7] after:transition-all after:duration-300 hover:after:w-full">
+                   <Link to="/contact">Contact</Link>
+              </li>
+              <a
+                href="/Barbara_Nkansah_Resume.pdf" target="_blank" rel="noopener noreferrer"
+                 className="relative cursor-pointer after:content-[''] after:block after:w-0 after:h-[2px] after:bg-[#00c9a7] after:transition-all after:duration-300 hover:after:w-full"
+              >
+                CV
+              </a>
+            </ul>
+          </nav> 
         </div>
       </header>
      
